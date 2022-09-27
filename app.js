5471dc1c34a380c3a701e02ca192beba3437da3b
brseghese <brseghese@gmail.com>
2022-09-27 11:33:23 -0300

    settings node_env

--- app.js
+++ app.js
@@ -8,7 +8,11 @@ const app = express();
 
 // ===== Middlewares ===== //
 
-app.use(morgan('dev'));
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
