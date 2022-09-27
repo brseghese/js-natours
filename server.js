c336567b71c3d8fb920b1b4ccb34696d762045a7
brseghese <brseghese@gmail.com>
2022-09-27 16:27:21 -0300

    eslint config

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
-
 const app = require('./app');
+
+dotenv.config({ path: './config.env' });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`App running on port ${port}`);
+  console.log(`App running on port ${port}...`);
 });
