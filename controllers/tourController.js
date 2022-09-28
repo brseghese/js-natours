17d4aa82c4e4a3f11cc69a24984646ce5c235553
brseghese <brseghese@gmail.com>
2022-09-28 14:35:37 -0300

    refactoring for MVC

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,20 +1,4 @@
-const express = require('express');
-const fs = require('fs');
-
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
-
-exports.checkID = (req, res, next, val) => {
-  console.log(`Tour id is: ${val}`);
-  if (req.params.id * 1 > tours.length - 1) {
-    return res.status(404).json({
-      status: 'Fail',
-      message: 'Invalid ID',
-    });
-  }
-  next();
-};
+// const Tour = require('../models/tourModel');
 
 exports.checkBody = (req, res, next) => {
   if (!req.body.name || !req.body.price)
@@ -26,55 +10,28 @@ exports.checkBody = (req, res, next) => {
 };
 
 exports.getAllTours = (req, res) => {
-  console.log(`Date: ${req.requestTime}`);
   res.status(200).json({
     status: 'success',
     requestedAt: req.requestTime,
-    results: tours.length,
-    data: {
-      tours: tours,
-    },
   });
 };
 
 exports.getTour = (req, res) => {
-  const id = req.params.id * 1;
-  const tour = tours.find((el) => el.id === id);
-
+  // const id = req.params.id * 1;
   res.status(200).json({
     status: 'success',
-    data: {
-      tour,
-    },
   });
 };
 
 exports.createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-
-  tours.push(newTour);
-
-  fs.writeFile(
-    `${__dirname}/../dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: newTour,
-        },
-      });
-    }
-  );
+  res.status(201).json({
+    status: 'success',
+  });
 };
 
 exports.updateTour = (req, res) => {
-  res.status(201).json({
+  res.status(200).json({
     status: 'success',
-    data: {
-      tour: '<Updated tour here...>',
-    },
   });
 };
 
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -3,8 +3,6 @@ const tourController = require('../controllers/tourController');
 
 const router = express.Router();
 
-router.param('id', tourController.checkID);
-
 router
   .route('/')
   .get(tourController.getAllTours)
