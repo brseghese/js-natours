3755db58e54af3ca66fc436b45289c71395df2d9
brseghese <brseghese@gmail.com>
2022-09-28 15:40:13 -0300

    create documents

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,13 +1,4 @@
-// const Tour = require('../models/tourModel');
-
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price)
-    return res.status(400).json({
-      status: 'Fail',
-      message: 'Missing name or price',
-    });
-  next();
-};
+const Tour = require('../models/tourModel');
 
 exports.getAllTours = (req, res) => {
   res.status(200).json({
@@ -23,10 +14,22 @@ exports.getTour = (req, res) => {
   });
 };
 
-exports.createTour = (req, res) => {
-  res.status(201).json({
-    status: 'success',
-  });
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid data sent!',
+    });
+  }
 };
 
 exports.updateTour = (req, res) => {
