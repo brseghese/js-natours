1765e42559321eb1c7f72ff9f61bfecf3cd12b7a
brseghese <brseghese@gmail.com>
2022-10-23 07:47:31 -0300

    logging in users with our api - part 1

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -13,7 +13,7 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getTour = catchAsync(async (req, res) => {
+exports.getTour = catchAsync(async (req, res, next) => {
   // 1) Get the data, for the requested tour (including reviews and guides)
   const tour = await Tour.findOne({ slug: req.params.slug }).populate({
     path: 'reviews',
@@ -22,17 +22,14 @@ exports.getTour = catchAsync(async (req, res) => {
 
   // 2) Build template
   // 3) Render template using data from 1)
-
-  res
-    .status(200)
-
-    .set(
-      'Content-Security-Policy',
-      "default-src 'self' https://*.mapbox.com ;base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src https://cdnjs.cloudflare.com https://api.mapbox.com 'self' blob: ;script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests;"
-    )
-
-    .render('tour', {
-      title: `${tour.name} Tour`,
-      tour,
-    });
+  res.status(200).render('tour', {
+    title: `${tour.name} Tour`,
+    tour,
+  });
 });
+
+exports.getLoginForm = (req, res) => {
+  res.status(200).render('login', {
+    title: 'Log into your account',
+  });
+};
--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,4 +1,5 @@
 /* eslint-disable */
+
 const locations = JSON.parse(document.getElementById('map').dataset.locations);
 console.log(locations);
 
