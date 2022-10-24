26eaf0b764a9b016f268ea47d1655df8cede13f6
brseghese <brseghese@gmail.com>
2022-10-24 18:16:45 -0300

    updating user data with the api

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,13 @@
 import '@babel/polyfill';
 import { displayMap } from './mapbox';
 import { login, logout } from './login';
+import { updateSettings } from './updateSettings';
 
 // Dom Elements
 const mapBox = document.getElementById('map');
 const loginForm = document.querySelector('.form--login');
 const logOutBtn = document.querySelector('.nav__el--logout');
+const userDataForm = document.querySelector('.form-user-data');
 
 // Delegation
 if (mapBox) {
@@ -23,3 +25,11 @@ if (loginForm)
   });
 
 if (logOutBtn) logOutBtn.addEventListener('click', logout);
+
+if (userDataForm)
+  userDataForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    updateSettings(name, email);
+  });
