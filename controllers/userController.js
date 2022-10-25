92f67e87c8f40d9687dafe6c346060cb18cf4773
brseghese <brseghese@gmail.com>
2022-10-25 14:57:40 -0300

    saving image name to database

--- controllers/userController.js
+++ controllers/userController.js
@@ -44,9 +44,6 @@ exports.getMe = (req, res, next) => {
 };
 
 exports.updateMe = catchAsync(async (req, res, next) => {
-  console.log(req.file);
-  console.log(req.body);
-
   // 1) Create error if user POSTs password data
   if (req.body.password || req.body.passwordConfirm) {
     return next(
@@ -59,6 +56,7 @@ exports.updateMe = catchAsync(async (req, res, next) => {
 
   // 2) Filtered out unwanted fields names that are not allowed to be updated
   const filteredBody = filterObj(req.body, 'name', 'email');
+  if (req.file) filteredBody.photo = req.file.filename;
 
   // 3) Update user document
   const updateUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
