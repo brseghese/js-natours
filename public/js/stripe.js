d6ad61c6fae36b3a5fb4de7dc094abf11dd48307
brseghese <brseghese@gmail.com>
2022-10-28 05:58:35 -0300

    processing payments on the frontend

---
+++ public/js/stripe.js
@@ -0,0 +1,25 @@
+/* eslint-disable */
+import axios from 'axios';
+import { showAlert } from './alerts';
+
+const stripe = Stripe(
+  'pk_test_51LxW3JBWi2w9Q2gCpcesP53GyW7uPoec1csKJAFmrZpA1KSR47rV2VLwkuOZZfREpkZkuSXRaJtACvrWXHunc46x00CwAP3bLj'
+);
+
+export const bookTour = async (tourId) => {
+  try {
+    // 1) Get checkout session from API
+    const session = await axios(
+      `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`
+    );
+
+    console.log(session);
+    // 2) Create checkout form + chanre credit card
+    await stripe.redirectToCheckout({
+      sessionId: session.data.session.id,
+    });
+  } catch (err) {
+    console.log(err);
+    showAlert('error', err);
+  }
+};
