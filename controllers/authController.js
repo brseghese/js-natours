7d2338c3f5849222e0a9dabf356b1e74a1bb1f98
brseghese <brseghese@gmail.com>
2022-10-29 11:13:02 -0300

    manipulating dates in authcontroller for brazil

--- controllers/authController.js
+++ controllers/authController.js
@@ -16,9 +16,9 @@ const createSendToken = (user, statusCode, res) => {
 
   const cookieOptions = {
     expires: new Date(
-      Date.now() +
-        process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000 -
-        3 * 60 * 60 * 1000
+      Date.now() -
+        3 * 60 * 60 * 1000 +
+        process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
   };
