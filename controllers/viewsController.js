7d2a5cc28d938efb64dc8f19667396f5ad86ce46
brseghese <brseghese@gmail.com>
2022-11-05 11:41:47 -0300

    building signup pug

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -35,6 +35,12 @@ exports.getTour = catchAsync(async (req, res, next) => {
   });
 });
 
+exports.getSignupForm = (req, res) => {
+  res.status(200).render('signup', {
+    title: 'Create your account',
+  });
+};
+
 exports.getLoginForm = (req, res) => {
   res.status(200).render('login', {
     title: 'Log into your account',
